1b6f0b943f5dbc3ddb5ce23c348942f5fda8dbfd
Johnkevin Ortega <slash2130kevin@gmail.com>
2023-04-16 21:31:31 -0500

    Terminado insertar lavadero datos + imagenes FUNCIONAL con SQL

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -5,28 +5,71 @@ const emailRegistro = require("../helpers/lavaderos/emailRegistro.js");
 const emailOlvidePassword = require("../helpers/emailOlvidePassword.js");
 const bcrypt = require("bcrypt");
 
-// Importaciones adicionales
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
+// Modelos
+const { ImagenLavadero } = require("../models/Lavadero.js");
 
-// Configuración de Cloudinary
-cloudinary.config({
-  cloud_name: 'dma9ouzwf',
-  api_key: '613731451848539',
-  api_secret: 'RaOglZL1K3XiItWXtmzZH0ENte4'
-});
+const registrarLavadero = async (req, res) => {
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'imagenes',
-    format: async (req, file) => 'jpg',
-    public_id: (req, file) => file.fieldname + '-' + Date.now(),
+  const { nombre, ciudad, direccion, telefono, correo_electronico, contrasena, horario_atencion } = req.body;
+
+  const token = generarId();
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(contrasena, salt);
+  const id = await bcrypt.hash(nombre + correo_electronico, salt);
+
+
+  let conexion;
+  try {
+    conexion = await conectarDB();
+
+    // Verificar si el usuario ya existe
+    const [row] = await conexion.execute(
+      `SELECT id_lavadero FROM lavaderos WHERE correo_electronico = ?`,
+      [correo_electronico]
+    );
+
+    if (row.length > 0) {
+      res.status(400).json({ msg: "El usuario ya existe" });
+      return;
+    }
+
+    await conexion.execute(
+      `INSERT INTO lavaderos (id_lavadero, nombre, ciudad, direccion, telefono, token, correo_electronico, contrasena, horario_atencion, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, true)`,
+      [id, nombre, ciudad, direccion, telefono, token, correo_electronico, hashedPassword, horario_atencion]
+    );
+
+    res.status(200).json({ msg: "Usuario registrado correctamente" });
+
+    // Si la inserción fue exitosa, guardar imagenes:
+    if (!req.files) {
+      res.status(500).send('Hubo un error al subir las imágenes');
+    } else {
+      const imageUrls = req.files.map((file) => file.path);
+      const sql = 'INSERT INTO imagenes_lavaderos (id_lavadero, ruta_imagen) VALUES ?';
+      const values = imageUrls.map((url) => [id, url]);
+          
+      await conexion.query(sql, [values], (error, result) => {
+        if (error) {
+          res.status(500).send('Hubo un error al guardar las imágenes en la base de datos');
+        }
+      });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hubo un error" });
+  } finally {
+    if (conexion) {
+      try {
+        await conexion.close();
+      } catch (error) {
+        console.log('Error al cerrar la conexión:', error);
+      }
+    }
   }
-});
+};
 
-const upload2 = multer({ storage: storage }).array('images');
+
 
 const loguearAdmin = async (req, res) => {
   const { correo_electronico, contrasena } = req.body;
@@ -68,75 +111,6 @@ const loguearAdmin = async (req, res) => {
   }
 };
 
-
-const registrarLavadero = async (req, res) => {
-  const { nombre, ciudad, direccion, telefono, correo_electronico, contrasena, horario_atencion } = req.body;
-  const images = req.files;
-  // Prevenir usuarios duplicados MYSQL
-  let conexion;
-  try {
-    conexion = await conectarDB();
-    // Verificar si el usuario ya existe
-    const [row] = await conexion.execute(
-      `SELECT id_lavadero FROM lavaderos WHERE correo_electronico = ?`,
-      [correo_electronico]
-    );
-
-    if (row.length > 0) {
-      res.status(400).json({ msg: "El usuario ya existe" });
-      return;
-    }
-
-    // Nueva línea
-    upload2(req, res, async (err) => {
-      if (err) {
-        res.status(500).send('Hubo un error al subir las imágenes');
-      } else {
-        const imageUrls = req.files.map((file) => file.path);
-        const sql = 'INSERT INTO imagenes (url) VALUES ?';
-        const values = imageUrls.map((url) => [url]);
-
-        await conexion.query(sql, [values], (error, result) => {
-          if (error) {
-            res.status(500).send('Hubo un error al guardar las imágenes en la base de datos');
-          } else {
-            res.status(200).send(result);
-          }
-        });
-      }
-    });
-
-
-    const token = generarId();
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(contrasena, salt);
-
-    await conexion.execute(
-      `INSERT INTO lavaderos (nombre, ciudad, direccion, telefono, token, correo_electronico, contrasena, horario_atencion, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, true)`,
-      [nombre, ciudad, direccion, telefono, token, correo_electronico, hashedPassword, horario_atencion]
-    );
-
-    emailRegistro({
-      email: correo_electronico,
-      nombre,
-      token,
-    });
-
-    res.status(200).json({ msg: "Usuario registrado correctamente" });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "Hubo un error" });
-  } finally {
-    if (conexion) {
-      try {
-        await conexion.close();
-      } catch (error) {
-        console.log('Error al cerrar la conexión:', error);
-      }
-    }
-  }
-};
 
 const getLavederos = async (req, res) => {
   let conexion;
