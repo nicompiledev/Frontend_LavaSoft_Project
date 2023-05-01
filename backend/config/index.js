deb32184b21b8315245c244cccc1a31fbf8f18d1
Johnkevin Ortega <slash2130kevin@gmail.com>
2023-04-30 20:25:01 -0500

    Eliminar Backend

--- backend/config/index.js
+++
@@ -1,5 +0,0 @@
-const conectarMySqlDB = require("./mysql.js");
-const conectarMongoDB = require("./mongodb.js");
-const conectarRedis = require("./redis.js");
-
-module.exports = { conectarMySqlDB, conectarMongoDB, conectarRedis };
